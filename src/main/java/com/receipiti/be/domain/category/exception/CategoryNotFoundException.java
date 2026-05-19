aa54a00067554b29fd7e7d92f7ecc08e73b0fc2b
bini0918 <165636716+bini0918@users.noreply.github.com>
2026-05-19 22:49:19 +0900

    feat: 지출내역 상세조회

--- src/main/java/com/receipiti/be/domain/category/exception/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.receipiti.be.domain.category.exception;
-
-public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-}
